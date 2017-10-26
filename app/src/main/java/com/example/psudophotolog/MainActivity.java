720f3f58d3b734169d2d5ee095cdd463b3ef4a96
dreamiland <maple_jista@hotmail.com>
2017-10-26 02:02:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/psudophotolog/MainActivity.java
@@ -0,0 +1,149 @@
+package com.example.psudophotolog;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView img1, img2, img3, img4, img5, img6, img7, img8, img9;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img1 = (ImageView) findViewById(R.id.img1);
+        img2 = (ImageView) findViewById(R.id.img2);
+        img3 = (ImageView) findViewById(R.id.img3);
+        img4 = (ImageView) findViewById(R.id.img4);
+        img5 = (ImageView) findViewById(R.id.img5);
+        img6 = (ImageView) findViewById(R.id.img6);
+        img7 = (ImageView) findViewById(R.id.img7);
+        img8 = (ImageView) findViewById(R.id.img8);
+        img9 = (ImageView) findViewById(R.id.img9);
+
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 2);
+            }
+        });
+
+        img4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 3);
+            }
+        });
+
+        img5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 4);
+            }
+        });
+
+        img6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 5);
+            }
+        });
+
+        img7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 6);
+            }
+        });
+
+        img8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 7);
+            }
+        });
+
+        img9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 8);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data.getExtras() != null) {
+            if (data.getExtras().get("data") != null) {
+                Bitmap bp = (Bitmap) data.getExtras().get("data");
+                switch (requestCode) {
+                    case 0:
+                        img1.setImageBitmap(bp);
+                        break;
+                    case 1:
+                        img2.setImageBitmap(bp);
+                        break;
+
+                    case 2:
+                        img3.setImageBitmap(bp);
+                        break;
+
+                    case 3:
+                        img4.setImageBitmap(bp);
+                        break;
+
+                    case 4:
+                        img5.setImageBitmap(bp);
+                        break;
+
+                    case 5:
+                        img6.setImageBitmap(bp);
+                        break;
+
+                    case 6:
+                        img7.setImageBitmap(bp);
+                        break;
+
+                    case 7:
+                        img8.setImageBitmap(bp);
+                        break;
+
+                    case 8:
+                        img9.setImageBitmap(bp);
+                        break;
+                }
+            }
+        }
+    }
+}
